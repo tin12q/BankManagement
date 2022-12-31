2a518f3d006ff2c0dc6d16daa06d16f5e1b7ce23
vkutin12q <thinhnp.22git@vku.udn.vn>
2022-12-31 21:38:10 +0700

    Create DataGet.java

---
+++ Client/src/main/java/tin/bank/client/model/DataGet.java
@@ -0,0 +1,13 @@
+package tin.bank.client.model;
+
+public class DataGet {
+    //mssql connection
+    private String url = "jdbc:sqlserver://localhost:1433;databaseName=Bank;integratedSecurity=true";
+    private String usr= "sa";
+    private String pss = "Abcd1234!";
+    private String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private void init(){
+
+    }
+
+}
