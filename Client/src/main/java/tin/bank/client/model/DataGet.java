0d199e88e097fc38cbca2375ef803daaaec7a932
vkutin12q <thinhnp.22git@vku.udn.vn>
2023-01-01 00:15:53 +0700

    Update DataGet.java

--- Client/src/main/java/tin/bank/client/model/DataGet.java
+++ Client/src/main/java/tin/bank/client/model/DataGet.java
@@ -1,13 +1,31 @@
 package tin.bank.client.model;
 
+import java.sql.*;
 public class DataGet {
     //mssql connection
-    private String url = "jdbc:sqlserver://localhost:1433;databaseName=Bank;integratedSecurity=true";
+    private String url = "jdbc:sqlserver://localhost:57000;databaseName=bank;integratedSecurity=true";
     private String usr= "sa";
     private String pss = "Abcd1234!";
     private String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private void init(){
+        try{
+            //sql server driver
+            Class.forName(driver);
+            //connection
+            Connection con = DriverManager.getConnection(url,usr,pss);
+            //statement
+            Statement st = con.createStatement();
+            //result
+            ResultSet rs = st.executeQuery("select * from account");
+            while(rs.next()){
+                System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
+            }
+        }catch (ClassNotFoundException e){
 
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
