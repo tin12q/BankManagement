bfcd27b8e75bd83dec80c7f720258cb46a6a3953
vkutin12q <thinhnp.22git@vku.udn.vn>
2023-01-01 14:40:47 +0700

    Create getUser in DataGet

--- Client/src/main/java/tin/bank/client/model/DataGet.java
+++ Client/src/main/java/tin/bank/client/model/DataGet.java
@@ -1,12 +1,14 @@
 package tin.bank.client.model;
 
 import java.sql.*;
+import java.util.LinkedList;
 public class DataGet {
     //mssql connection
     private static String url = "jdbc:sqlserver://localhost:57000;databaseName=Bank;encrypt=true;trustServerCertificate=true ";
     private static String usr= "sa";
     private static String pss = "Abcd1234!";
     private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static LinkedList<Account> accounts = new LinkedList<>();
     private void init(){
         try{
             //sql server driver
@@ -52,5 +54,43 @@ public class DataGet {
         }
         return false;
     }
+    // TODO: 2023-01-01
+    // 1. Add a method to get all the accounts
+    // 2. Add a method to get all the transactions
+    // 3. Add a method to get all the users
+    // 4. Output the result to UI 
 
+    // Get all the user to a list
+    public LinkedList<Account> getAccounts(){
+        return accounts;
+    }
+    public static void addAccount(Account account){
+        accounts.add(account);
+    }
+    public static void getUsers(){
+        try{
+            //sql server driver
+            Class.forName(driver);
+            //connection
+            Connection con = DriverManager.getConnection(url,usr,pss);
+            //statement
+            Statement st = con.createStatement();
+            //result
+            
+            ResultSet rs = st.executeQuery("select * from account");
+            
+
+            while(rs.next()){
+                Account account = new Account(rs.getString("id"),rs.getString("name"),rs.getDouble("balance"));
+                addAccount(account);
+            }
+            
+            
+        }catch (ClassNotFoundException e){
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+    }
+    }
 }
